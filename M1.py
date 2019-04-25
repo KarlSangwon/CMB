c79d9ba70f6ca38b074e630cbcadb25636730f19
KarlSangwon <49044902+KarlSangwon@users.noreply.github.com>
2019-04-25 14:37:37 -0400

    Update M1.py

--- M1.py
+++ M1.py
@@ -1,11 +1,5 @@
-
 #Karl CMB Project
-
-# Karl
-# M1
-
-#Update
-#TODO: Current Issue. Label and Conv Output dimension does not match. Need to map back to original dimension. (algorithm in paper)
+# M1 - SWI Training
 
 # Imports
 
@@ -17,26 +11,11 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
+import numpy as np
 import pdb
-#Karl CMB Project
 
-# Karl
-# M1
 
-#Update
-#TODO: Current Issue. Label and Conv Output dimension does not match. Need to map back to original dimension. (algorithm in paper)
 
-# Imports
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.data as data
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-
-import pdb
 # Hyperparameters
 num_epochs = 5
 num_classes = 2
@@ -46,6 +25,24 @@ learning_rate = 0.03
 momentum = 0.9
 dropout_rate = 0.3
 # 0. Loading Data
+
+ROI_total = np.load('isoCMB.npy').item()
+print("Number of Subjects (SWI Contrast):",len(ROI_total))
+
+ROI = []
+
+for i in range(len(ROI_total)):
+    print("Sample Pixel Value of 1st CMB for subject N%s at (0,0,0):"%(i+1), ROI_total['N%s'%(i+1)][0][0][0][0])
+    
+    #indexing: (cmb#, x, y, z)
+    
+    # merge all subjects' CMB into one array
+    for j in range(len(ROI_total['N%s'%(i+1)])):
+        ROI.append(ROI_total['N%s'%(i+1)][j])
+
+#ROI: 74 SWI CMBs. 
+
+#We will partition 74 into 56 train / 18 test.
 
 # 0.A. Creating a Fake Dataset for Experimentation
 
@@ -68,10 +65,9 @@ class FakeDataset3D(data.Dataset):
         #img = torch.randn(*self.image_size)
         img = torch.randint(0, 256, size = self.image_size)
         #target = torch.randint(0, self.num_classes, size=(1,), dtype=torch.long)[0]
-        target = torch.randint(0, self.num_classes, size = (27,27,28), dtype = torch.long)
+        target = torch.randint(0, self.num_classes, size = (27,27,27), dtype = torch.long)
         #target = torch.randint(0, self.num_classes, size = self.image_size)
         torch.set_rng_state(rng_state)
-​
         # convert to PIL Image
         #img = transforms.ToPILImage()(img)
         if self.transform is not None:
@@ -92,12 +88,13 @@ class FakeDataset3D(data.Dataset):
         tmp = '    Target Transforms (if any): '
         fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
         return fmt_str
-#currently empty
+    
+    
+#0.B SWI dataset for CMB    
 
 class TrainDataset(data.Dataset):
-    def __init__(self):
-        # TODO
-        # 1. Initialize file paths or a list of file names.
+    def __init__(self, size = 56, image_size = (1,16,16,16), num_classes = 2, transform = None, target_transform = None, random_offset =0):
+        
         pass
     def __getitem__(self, index):
         # TODO
@@ -124,7 +121,9 @@ class TestDataset(torch.utils.data.Dataset):
     def __len__(self):
         # Change 0 to the total size of your dataset.
         return 0
-# Data loader.
+    
+    
+    # Data loader.
 #train_dataset = TrainDataset()
 #train_dataset = torchvision.datasets.FakeData(size=100, image_size=(64,64,3,1), num_classes=2, transform=transforms.ToTensor(), target_transform =None,random_offset=0)
 train_dataset = FakeDataset3D(size = 1, image_size = (1,64,64,64), num_classes=2, transform = None, target_transform = None, random_offset = 0)
@@ -142,10 +141,10 @@ for i, (images, labels) in enumerate(test_loader):
 class M1(nn.Module):
     def __init__(self):
         super(M1, self).__init__()
-        self.conv1 = nn.Sequential(nn.Conv3d(1,64, (5,5,3)), nn.Dropout(0.2)) # in_channel, out_channel, kernel
+        self.conv1 = nn.Sequential(nn.Conv3d(1,64, (5,5,5)), nn.Dropout(0.2)) # in_channel, out_channel, kernel
         self.pool = nn.MaxPool3d((2, 2, 2),2) # kernel, stride
         self.conv2 = nn.Sequential(nn.Conv3d(64, 64, (3,3,3)), nn.Dropout(0.3)) # in_channel, out_channel, kernel
-        self.conv3 = nn.Sequential(nn.Conv3d(64, 64, (3,3,1)), nn.Dropout(0.3)) # in_channel, out_channel, kernel
+        self.conv3 = nn.Sequential(nn.Conv3d(64, 64, (3,3,3)), nn.Dropout(0.3)) # in_channel, out_channel, kernel
         self.fc1 = nn.Sequential(nn.Conv3d(64,150, (2,2,2)), nn.Dropout(0.3))
         self.fc2 = nn.Sequential(nn.Conv3d(150,2, (1,1,1)), nn.Dropout(0.3))
 
@@ -162,10 +161,7 @@ class M1(nn.Module):
         return x
 
 m1 = M1()
-# 2. Loss and Optimizer
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(m1.parameters(), lr = learning_rate)
 # 3. Training Loop
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -187,16 +183,7 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-torch.Size([1, 2, 27, 27, 28])
-Epoch [1/5], Step [1/1], Loss: 1.1786
-torch.Size([1, 2, 27, 27, 28])
-Epoch [2/5], Step [1/1], Loss: 0.6991
-torch.Size([1, 2, 27, 27, 28])
-Epoch [3/5], Step [1/1], Loss: 0.6931
-torch.Size([1, 2, 27, 27, 28])
-Epoch [4/5], Step [1/1], Loss: 0.6931
-torch.Size([1, 2, 27, 27, 28])
-Epoch [5/5], Step [1/1], Loss: 0.6931
+
 # 4. Testing
 
 m1.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
